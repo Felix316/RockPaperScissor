a82d66e8627bec9a4f082b523765f2df0bdb0fce
Gil Felix <gfelix316@gmail.com>
2023-01-06 03:33:55 -0500

    Updated RPS

--- javascript.js
+++ javascript.js
@@ -1,48 +1,77 @@
+let playerScore = 0;
+let computerScore = 0;
+let annonce = "";
+let result = "";
+let weapons = "";
 
-  const playerText = document.querySelector("#playerText");
-  const computerText = document.querySelector("#computerText");
-  const resultText = document.querySelector("#resultText");
-  const choiceBtns = document.querySelectorAll(".choiceBtn");
-  let player;
-  let computer;
-  let result;
-
-  choiceBtns.forEach(button => button.addEventListener("click", () => {
-
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = checkWinner();
-  }));
-
-  function computerTurn(){
-
-        const randNum = Math.floor(Math.random() * 3) + 1;
-
-        switch(randNum){
-            case 1:
-                computer = "ROCK";
-                break;
-            case 2:
-                computer = "PAPER";
-                break;
-            case 3:
-                computer = "SCISSORS";
-                break;
+weapons = document.querySelectorAll('.weapon');
+weapons.forEach(img => {
+    img.addEventListener('click', function () {
+        if (playerScore < 5 && computerScore < 5) {
+            playRound(img.id);
         }
-  }
-  function checkWinner(){
-    if(player == computer){
-        return "Draw!";
+    })
+})
+
+function updateResults() {
+    result = document.getElementById("player");
+    result.innerHTML = playerScore;
+    result = document.getElementById("computer");
+    result.innerHTML = computerScore;
+}
+
+function updateAnnonce() {
+    let txt = "";
+    txt = document.getElementById("roundResult");
+    txt.innerHTML = annonce;
+}
+
+play.addEventListener('click', function() {location.reload()});
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    if (playerSelection === computerSelection) {
+        annonce = "This is a Tie! Player: " + playerSelection + ", Computer: " + computerSelection;
+        updateAnnonce();
     }
-    else if(computer == "ROCK"){
-        return (player == "PAPER") ? "You Win!" : "You Lose!"
+    else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+        annonce = "You Win! " + playerSelection + " beats " + computerSelection;
+        updateAnnonce();
+        playerScore += 1;
+        updateResults();
+        if ((playerScore == 5) || (playerScore > 5)) {
+            let finalResult = document.getElementById("summary");
+            finalResult.innerHTML = "You won the Game! Congratulations!";
+            finalResult = document.getElementById("continue");
+            finalResult.innerHTML = "If you want to play more, please, press the button!";
+            document.getElementById("play").disabled = false;
+        }
     }
-    else if(computer == "PAPER"){
-        return (player == "SCISSORS") ? "You Win!" : "You Lose!"
+    else {
+        annonce = "You Lost! " + computerSelection + " beats " + playerSelection;
+        updateAnnonce();
+        computerScore += 1;
+        updateResults();
+        if ((computerScore == 5) || (computerScore > 5)) {
+            let finalResult = document.getElementById("summary");
+            finalResult.innerHTML = "You lost the Game! Play again!";
+            finalResult = document.getElementById("continue");
+            finalResult.innerHTML = "If you want to play more, please, press the button!";
+            document.getElementById("play").disabled = false;
+        }
     }
-    else if(computer == "SCISSORS"){
-        return (player == "ROCK") ? "You Win!" : "You Lose!"
+}
+
+function computerPlay() {
+    compChoise = Math.floor(Math.random() * 3);
+    switch (compChoise) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
-  }
+}
